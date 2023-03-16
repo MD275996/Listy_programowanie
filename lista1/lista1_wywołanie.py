84d168939edbf95886bdd45fc1247fb9cd2a58d0
MD275996 <127320680+MD275996@users.noreply.github.com>
2023-03-16 18:32:02 +0100

    Update lista1_wywołanie.py

--- lista1/lista1_wywołanie.py
+++ lista1/lista1_wywołanie.py
@@ -1,54 +1,36 @@
 from lista1 import Vector
 
 v = Vector()
+
+v.impl_list([1,2,"a"])
+v.impl_list([1,2,3,4])
 v.impl_list([1,2,3])
-
-def tworzenie_skalara():
-    #kod sprawdzający kropki 1, 2, 3, 
-    
-    s = Vector(5)
-    print(v)
-    print(s)
-
-    v.rand_gen()
-    print(v)
-
-    v.impl_list([1,2,3,4])
-    v.impl_list([1,2,3])
-    print(v)
-
-def dodawanie_wektorow():
-    #kod sprawdza kropkę 4
-
-    u = Vector()
-    u.impl_list([100,100,100])
-    print(v+u)
-    print(v + [1,2,3,4])
-
-def mnozenie_skalar():
-
-    print(v*12)
-    print(v*"asd")
-
-def dlugosc_suma():
-    #kod sprawdza kropki 6,7
-
-    print(v.length())
-    print(v.sum_elem())
-
-def iloczyn_sk():
-    u = Vector()
-    u.impl_list([100,100,100])
-    print(v.iloc_skal([4,4,4]))
-    print(v.iloc_skal(u))
-
-def elementy():
-    #kod sprawdza kropki 10, 11
-    print(v[0])
-    v[0] = 10
-    print(v)
-    print(10 in v)
-
-
-v.impl_list([1, 2 ,'c'])
 print(v)
+
+print("===========================================================")
+
+v.rand_gen()
+print(v)
+
+print("===========================================================")
+
+u = Vector()
+u.rand_gen()
+
+print(v+u)
+print(v-u)
+print(v*2)
+
+print("===========================================================")
+
+print(u.length())
+print(u.sum_elem())
+print(v.iloc_skal(u))
+
+print("===========================================================")
+
+print(u)
+print(u[0])
+u[0]=1
+print(u)
+print(1 in u)
