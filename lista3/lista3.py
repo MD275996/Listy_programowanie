4a7a0a2e63a80ef6c09b26050c47524c8342a01c
R3gx-00 <56203102+R3gx-00@users.noreply.github.com>
2023-04-13 16:55:15 +0200

    commit dla pulla

---
+++ lista3/lista3.py
@@ -0,0 +1,22 @@
+from zipfile import ZipFile
+from datetime import date
+import PyPDF2 as pypdf
+
+def szukanie_plików(rozszrz, katalogi):
+    return 0                #poziom trudnosci: 3
+
+def zmiana_znaku():         #poziom trudnosci: 3
+    return 0
+
+
+def merge_pdf(dokumenty):   #poziom trudnosci: 1
+    return 0
+
+def kody():
+    return 0                #poziom trudnosci: 2
+
+def nawiasy():              #poziom trudnosci: 1
+    return 0
+
+def web_scrap():            #poziom trudnosci: 2
+    return 0
