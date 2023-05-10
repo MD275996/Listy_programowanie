6ffdd2b2630b167f627156c2423c1d6a30c3d354
validName <>
2023-05-10 16:39:07 +0200

    skonczenie grafów

--- lista4/graf_losowy.py
+++ lista4/graf_losowy.py
@@ -72,7 +72,7 @@ def random_graph(n,p):
     frame_one.save("chodzący_agent.gif", format="GIF", append_images = frames, save_all=True, duration=n*30, loop=0)
 
     #usuwamy zdjęcia potrzebne do gifa
-    folder = "C:\\Users\\marek\\Desktop\\Listy_programowanie\\lista4\\zdj_bldz_lsw"
+    folder = os.path.join(os.getcwd(),"zdj_bldz_lsw")
     for filename in os.listdir(folder):
         filepath = os.path.join(folder,filename)
         os.remove(filepath)
@@ -81,10 +81,111 @@ def random_graph(n,p):
 # ==============================================================================================================================================================
 def watts_strogatz_graph(n,k,p):
     G = nx.watts_strogatz_graph(n,k,p)
-    ax = plt.subplot()     
-    nx.draw(G,with_labels=True)
-    plt.show()
+    pos = nx.spring_layout(G)
 
+    
+    ax = plt.subplot()     
+    
+    #stwarzamy własną listę kolorów
+    my_node_list_colors=["blue" for el in G.nodes]
+
+    #wybór losowego elementu
+    agent = rand.choice(list(G.nodes))
+
+    my_node_list_colors[agent] = "orange"
+
+
+
+   
+    nx.draw_networkx_nodes(G,pos,node_color=my_node_list_colors)            #rysujemy nasz graf z aktywnym elementem
+    nx.draw_networkx_edges(G,pos)
+    nx.draw_networkx_labels(G,pos)
+    plt.savefig("zdj_bldz_lsw\\fig0.png")
+
+    #ZMIANA AGENTA
+    for i in range(1,n+1):
+        stary_agent = agent
+        adj = list(G.adj[agent].keys())                                                   #sąsiedzi naszego losowego agenta     
+        agent = rand.choice(adj)                                                          #nowy agent
+
+        my_node_list_colors[stary_agent]="blue"
+        my_node_list_colors[agent]="orange"
+
+        nx.draw_networkx_nodes(G,pos,node_color=my_node_list_colors)            #rysujemy nasz graf z aktywnym elementem
+        nx.draw_networkx_edges(G,pos)
+        nx.draw_networkx_labels(G,pos)
+        
+        plt.savefig("zdj_bldz_lsw\\fig"+str(i)+".png")
+        plt.clf()
+
+
+    #tworzenie gifa
+    pictures = ["zdj_bldz_lsw\\fig"+str(i)+".png" for i in range(0,n+1)]
+    frames = [Image.open(image) for image in pictures]
+    frame_one = frames[0]
+    frame_one.save("chodzący_agent_watts_strogatz_graph.gif", format="GIF", append_images = frames, save_all=True, duration=n*30, loop=0)
+
+    #usuwamy zdjęcia potrzebne do gifa
+    folder = os.path.join(os.getcwd(),"zdj_bldz_lsw")
+    for filename in os.listdir(folder):
+        filepath = os.path.join(folder,filename)
+        os.remove(filepath)
+    
+
+
+
+
+def barabasi_albert_graph(n,k):
+    G = nx.barabasi_albert_graph(n,k)
+    pos = nx.spring_layout(G)
+
+    ax = plt.subplot()     
+    
+    #stwarzamy własną listę kolorów
+    my_node_list_colors=["blue" for el in G.nodes]
+
+    #wybór losowego elementu
+    agent = rand.choice(list(G.nodes))
+
+    my_node_list_colors[agent] = "orange"
+
+    nx.draw_networkx_nodes(G,pos,node_color=my_node_list_colors)            #rysujemy nasz graf z aktywnym elementem
+    nx.draw_networkx_edges(G,pos)
+    nx.draw_networkx_labels(G,pos)
+    plt.savefig("zdj_bldz_lsw\\fig0.png")
+
+    
+    #ZMIANA AGENTA
+    for i in range(1,n+1):
+        stary_agent = agent
+        adj = list(G.adj[agent].keys())                                                   #sąsiedzi naszego losowego agenta     
+        agent = rand.choice(adj)                                                          #nowy agent
+
+        my_node_list_colors[stary_agent]="blue"
+        my_node_list_colors[agent]="orange"
+
+        nx.draw_networkx_nodes(G,pos,node_color=my_node_list_colors)            #rysujemy nasz graf z aktywnym elementem
+        nx.draw_networkx_edges(G,pos)
+        nx.draw_networkx_labels(G,pos)
+        
+        plt.savefig("zdj_bldz_lsw\\fig"+str(i)+".png")
+        plt.clf()
+
+
+    #tworzenie gifa
+    pictures = ["zdj_bldz_lsw\\fig"+str(i)+".png" for i in range(0,n+1)]
+    frames = [Image.open(image) for image in pictures]
+    frame_one = frames[0]
+    frame_one.save("chodzący_agent_barabasi_albert_graph.gif", format="GIF", append_images = frames, save_all=True, duration=n*30, loop=0)
+
+    #usuwamy zdjęcia potrzebne do gifa
+    folder = os.path.join(os.getcwd(),"zdj_bldz_lsw")
+    for filename in os.listdir(folder):
+        filepath = os.path.join(folder,filename)
+        os.remove(filepath)
+    
 
 #random_graph(7,0.4)
+#watts_strogatz_graph(10,4,0)
+barabasi_albert_graph(10,3)
 #umożliwić wykonywanie z linii komend
